6c172f4bd7cf4a9bfea830b6f74554b4f4352427
Aryan Patel <aryan.ajitkumar.patel@gmail.com>
2026-07-01 13:16:29 +0530

    Update ExpenseController.java

--- backend/src/main/java/com/aryan/expensetracker/ExpenseController.java
+++ backend/src/main/java/com/aryan/expensetracker/ExpenseController.java
@@ -3,7 +3,6 @@ package com.aryan.expensetracker;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/expenses")
 public class ExpenseController {
